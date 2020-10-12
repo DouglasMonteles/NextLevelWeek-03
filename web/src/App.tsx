e1dda985387ab183f370c8b32f501b6e99a49285
DouglasMonteles <silvadouglas328@gmail.com>
2020-10-12 19:27:12 -0300

    Criando a Landing Page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import { FiArrowRight } from 'react-icons/fi';
+
+import './styles/global.css';
+import './styles/pages/landing.css';
+
+import logoImg from './assets/logo.svg';
 
 function App() {
   return (
-    <h1>Works!</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Logo da aplicacao Happy"/>
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>Rio do Sul</strong>
+          <span>Santa Catarina</span>
+        </div>
+
+        <a href="" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0,0,0,.6)" />
+        </a>
+      </div>
+    </div>
   );
 }
 
