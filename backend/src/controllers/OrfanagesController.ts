2782bdb2a96f6b47faf3aded9fea738a29f007ed
DouglasMonteles <silvadouglas328@gmail.com>
2020-10-14 12:09:33 -0300

    Finalizando o upload de imagens

--- backend/src/controllers/OrfanagesController.ts
+++ backend/src/controllers/OrfanagesController.ts
@@ -38,6 +38,11 @@ export default {
 
     const orphanageRepository = getRepository(Orphanage);
 
+    const requestImages = request.files as Express.Multer.File[];
+    const images = requestImages.map(image => {
+      return { path: image.filename };
+    });
+
     const orphanage = orphanageRepository.create({
       name,
       latitude,
@@ -46,6 +51,7 @@ export default {
       instructions,
       opening_hours,
       open_on_weekends,
+      images,
     });
 
     await orphanageRepository.save(orphanage);
